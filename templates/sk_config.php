519546457e63508fe8a939644442a113be3fb236
sebaxtian <sebaxtian@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-13 13:39:49 +0000

    * Solved a Bug in the configuration form that asked for Minimax. * Solved a Bug in the configuration form that asked for an API key even if there is a valid one.
    git-svn-id: https://plugins.svn.wordpress.org/schreikasten/trunk@251675 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/sk_config.php
+++ templates/sk_config.php
@@ -2,11 +2,9 @@
 	<h2><?php _e('Schreikasten Configuration', 'sk') ?></h2>
 	<form name="form1" method="post" action="<?php echo remove_query_arg(array('mode', 'id')); ?>">
 		<table align="center">
-			<tr><td colspan="3"><div class='updated'><p>
-					<?php
-			if(!function_exists('minimax_version') || minimax_version()<SK_MNMX_V) { printf(__('You have to install <a href="%s" target="_BLANK">minimax %1.1f</a> in order for this plugin to work.', 'mudslide'), "http://wordpress.org/extend/plugins/minimax/", SK_MNMX_V ); }?></p>
-				<p><?php _e("You need an Akismet <a href='http://wordpress.com/api-keys/' target='_BLANK'>API</a> to enable the antispam filter.", 'sk' );?></p></div>
-			</td></tr>
+			<?php if(strlen(get_option('sk_api_key'))==0 || !sk_verify_key()) {
+				echo "<tr><td colspan='3'><div class='updated'><p>".__("You need an Akismet <a href='http://wordpress.com/api-keys/' target='_BLANK'>API</a> to enable the antispam filter.", 'sk' )."</p></div></td></tr>"; 
+			}?>
 			<tr>
 				<td><?php _e("Akismet API", 'sk' ); ?>:</td>
 				<td><input type="text" name="sk_api_key" size="30" value="<?php echo $sk_api_key; ?>" /></td>
