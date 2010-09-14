92dbf7eccb1f0058d9d4c8113bc584dcd6d1f117
sebaxtian <sebaxtian@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-14 23:30:43 +0000

    Solved i18n situation.
    git-svn-id: https://plugins.svn.wordpress.org/schreikasten/trunk@289571 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/sk_config.php
+++ templates/sk_config.php
@@ -27,7 +27,12 @@
 
 <tr>
 	<td><?php _e("Date format", 'sk' ); ?>: </td>
-	<td><input type='text' name='date_format' value='<?php echo $options['date_format']; ?>'> <a href='http://codex.wordpress.org/Formatting_Date_and_Time' target='_BLANK'><?php _e('Doumentation about date format.', 'sk'); ?></a></td>
+	<td><input type='text' name='date_format' value='<?php echo $options['date_format']; ?>'></td>
+</tr>
+
+<tr>
+	<td></td>
+	<td><a href='http://codex.wordpress.org/Formatting_Date_and_Time' target='_BLANK'><?php _e('Doumentation about date format.', 'sk'); ?></a></td>
 </tr>
 
 <tr>
